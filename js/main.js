5cf9c1ac89a66419913f538ecb0444fd47a991ac
tikhele <tikhele@gmail.com>
2022-02-02 13:47:50 +0300

    create: style for reviews

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ var hotelSlider = new Swiper('.hotel-slider', {
   },
   
 });
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+    loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  
+});
